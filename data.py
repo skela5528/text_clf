ac6d12bd35214cd2f0cca11f567e2cd4479759fe
skela5528 <skela@yandex.ru>
2021-03-27 12:25:13 +0100

    data.py:  - fix training split  - add no_topic label  - comments  - write_data()

--- data.py
+++ data.py
@@ -21,9 +21,11 @@ class Preprocessor:
     BODY_ID = 4
     TEXT_TAG = 'text'
     TITLE_TAG = 'title'
-    TRAIN_TAG = 'TRAIN'
+    TRAIN_TAG = 'lewissplit'
+    TRAIN_SPLIT_STRING = 'TRAIN'
     DOC_TAG = 'reuters'
     STATISTICS_OUT_DIR = 'statistics_out'
+    NO_TOPIC = 'NO_TOPIC'
 
     def __init__(self):
         self.stop_words = set(stopwords.words('english'))  # type: Set[str]
@@ -54,9 +56,12 @@ class Preprocessor:
         for sentence in sent_tokenize(text):
             sentence_tokens = []
             for tok in self.tokenizer.tokenize(sentence):
+                # lower
                 tok = tok.lower()
+                # stop words
                 if remove_stop_words and tok in self.stop_words:
                     continue
+                # lemmatization
                 if lemmatization:
                     tok = self.lemmatizer.lemmatize(tok)
                 sentence_tokens.append(tok)
@@ -69,9 +74,13 @@ class Preprocessor:
         return sep.join(strings)
 
     @staticmethod
-    def write_data(docs: List[str], topics: List[str]):
-        assert len(docs) == len(topics)
-        pass
+    def write_data(docs: List[str], topics: List[str], ids: List[str], out_path: str, sep: str = ','):
+        assert len(docs) == len(topics) == len(ids)
+        LOGGER.info(f'[data] writing normalized data to {os.path.abspath(out_path)}')
+        with open(out_path, mode='a') as stream:
+            for doc, doc_topics, doc_newid in zip(docs, topics, ids):
+                line = f'{doc}{sep}{doc_topics}{sep}{doc_newid}\n'
+                stream.write(line)
 
     @staticmethod
     def read_data():
@@ -103,31 +112,32 @@ class Preprocessor:
         return body
 
     @staticmethod
-    def _get_topics(document_tag: Tag) -> List[str]:
+    def _get_topics(document_tag: Tag, no_topic: str) -> List[str]:
         topics = [topic_tag.text for topic_tag in document_tag.topics.contents]
+        topics = topics if len(topics) else [no_topic]
         return topics
 
     @staticmethod
     def _get_is_train_split(document_tag: Tag) -> bool:
-        is_train_split = document_tag.lewissplit == Preprocessor.TRAIN_TAG
+        is_train_split = document_tag[Preprocessor.TRAIN_TAG] == Preprocessor.TRAIN_SPLIT_STRING
         return is_train_split
 
     def preprocess_reuters_files(self, reuters_raw_path: str, min_doc_length: int = 1, save_stats: bool = False):
         assert os.path.exists(reuters_raw_path), LOGGER.warn(f'Reuters dataset not found at: {reuters_raw_path}!')
-        train_docs, train_topics = [], []
-        test_docs, test_topics = [], []
+        train_ids, train_docs, train_topics = [], [], []
+        test_ids, test_docs, test_topics = [], [], []
 
         chunk_paths = self._get_chunk_paths(reuters_raw_path)
-        for chunk_file_path in chunk_paths:
-            LOGGER.info(f"reading: {os.path.basename(chunk_file_path)}")
-            with open(chunk_file_path, mode="rb") as chunk_reader:
+        for chunk_i, chunk_file_path in enumerate(chunk_paths):
+            LOGGER.info(f'[data] reading: {os.path.basename(chunk_file_path)}')
+            with open(chunk_file_path, mode='rb') as chunk_reader:
                 data = chunk_reader.read()
-                data = data.decode("utf-8", "ignore")
+                data = data.decode('utf-8', 'ignore')
 
-            parsed_data = BeautifulSoup(data, features="lxml")
+            parsed_data = BeautifulSoup(data, features='lxml')
             document_tags = parsed_data.findAll(Preprocessor.DOC_TAG)
 
-            for i, doc in enumerate(document_tags):
+            for doc_i, doc in enumerate(document_tags):
                 # parse title, body and topics
                 # title = self._get_title(doc)  # TODO test with titles
                 body = self._get_body(doc)
@@ -135,7 +145,7 @@ class Preprocessor:
                 # skip too short docs also docs without body
                 if len(norm_body) < min_doc_length:
                     continue
-                topics = self._get_topics(doc)
+                topics = self._get_topics(doc, self.NO_TOPIC)
                 is_train_split = self._get_is_train_split(doc)
                 self._update_statistics(doc, topics, is_train_split)
 
@@ -145,26 +155,35 @@ class Preprocessor:
                 if is_train_split:
                     train_docs.append(norm_body)
                     train_topics.append(topics)
+                    train_ids.append(doc['newid'])
                 else:
                     test_docs.append(norm_body)
                     test_topics.append(topics)
+                    test_ids.append(doc['newid'])
 
-                # print(title, ' - ', post_id, ' - ', is_train_split)
-                # print(topics)
-                # print(body, '\n\n')
-                # if int(i) > 5:
-                #     break
         self._save_statistics(save_stats)
-        return (train_docs, train_topics), (test_docs, test_topics)
+        return (train_docs, train_topics, train_ids), (test_docs, test_topics, test_ids)
 
     def preprocess_text_file(self, text_file_path: str):
         pass
 
 
-if __name__ == '__main__':
-    reuters_raw_path_ = CONFIG['DATA']['reuters_raw_path']
-    min_doc_length_ = CONFIG['DATA']['min_doc_length']
-    save_stats_ = CONFIG['DATA']['save_stats']
+def prepare_data():
+    # parse config
+    reuters_raw_path = CONFIG.get('DATA', 'reuters_raw_path')
+    min_doc_length = CONFIG.getint('DATA', 'min_doc_length')
+    save_stats = CONFIG.get('DATA', 'save_stats')
+    train_prep_data_path = CONFIG.get('DATA', 'reuters_prep_train_path')
+    test_prep_data_path = CONFIG.get('DATA', 'reuters_prep_test_path')
 
+    # normalize data
     prep = Preprocessor()
-    train, test = prep.preprocess_reuters_files(reuters_raw_path_, min_doc_length_, save_stats_)
+    train, test = prep.preprocess_reuters_files(reuters_raw_path, min_doc_length, save_stats)
+
+    # save preprocessed data
+    prep.write_data(*train, train_prep_data_path)
+    prep.write_data(*test, test_prep_data_path)
+
+
+if __name__ == '__main__':
+    prepare_data()
