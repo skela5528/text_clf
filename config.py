cfaf93df9175ac00a377ac75ac6430a14e914f8f
skela5528 <skela@yandex.ru>
2021-03-29 17:55:10 +0200

    config.py: exit if not found default config.ini

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
+import os
 from configparser import ConfigParser
+
+from constants import LOGGER
+
+CONFIG_SAMPLE_URL = 'https://github.com/skela5528/text_clf/blob/main/config.ini'
 
 
 def get_config(config_path: str = 'config.ini') -> ConfigParser:
-    config = ConfigParser()
-    config.read(config_path)
-    print(config.sections())
-    # TODO validate sections
-    return config
+    global CONFIG
+    if not os.path.exists(config_path):
+        full_config_path = os.path.abspath(config_path)
+        LOGGER.error(f'[config] Config not found at {full_config_path}! Take config from {CONFIG_SAMPLE_URL}.')
+        exit()
+    CONFIG = ConfigParser()
+    CONFIG.read(config_path)
+    return CONFIG
 
 
 def parse_config_section(config_section) -> dict:
@@ -17,5 +25,5 @@ def parse_config_section(config_section) -> dict:
         params_dict[param] = value
     return params_dict
 
-global CONFIG
+
 CONFIG = get_config()
