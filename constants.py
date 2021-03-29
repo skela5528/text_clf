96046e7a249f143c496269f749db3c539cce3eaa
skela5528 <skela@yandex.ru>
2021-03-29 10:48:36 +0200

    constants.py: add topic counts

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+import numpy as np
+
 
 def get_logger(log_path: str = None, level=logging.DEBUG) -> logging.Logger:
     global LOGGER
@@ -36,6 +38,13 @@ TOPICS = ['NO_TOPIC', 'earn', 'acq', 'money-fx', 'grain', 'crude', 'trade', 'int
           'lin-oil', 'rye', 'red-bean', 'groundnut-oil', 'citruspulp', 'rape-meal', 'can', 'dfl', 'corn-oil', 'peseta',
           'cotton-oil', 'nzdlr', 'rand', 'ringgit', 'castorseed', 'castor-oil', 'lit', 'rupiah', 'skr', 'nkr', 'dkr',
           'sun-meal', 'lin-meal']
+
+TOPICS_COUNTS = np.array(
+    [5540, 2687, 1447, 397, 371, 309, 281, 254, 192, 178, 148, 108, 108, 97, 92, 83, 80, 80, 79, 70, 65, 63,
+     55, 54, 48, 46, 44, 39, 37, 36, 35, 34, 33, 32, 31, 31, 29, 29, 28, 26, 21, 21, 20, 18, 18, 16, 16, 16,
+     15, 15, 14, 14, 14, 13, 13, 12, 12, 10, 9, 9, 8, 8, 7, 7, 6, 5, 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3, 3,
+     3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+     1, 1])
 
 TOPICS_STRING = ' '.join(TOPICS)
 
